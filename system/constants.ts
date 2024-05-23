f0db02732ac95f67286d435e3988c1bf0701c8fc
selimdoyranli <selimdoyranli@gmail.com>
2024-05-23 22:43:05 +0300

    revert: remove dotenv module

--- system/constants.ts
+++ system/constants.ts
@@ -1,3 +1,4 @@
+export const API = 'https://backend.revilink.io/api'
 export const USERNAME_REGEX = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/
 export const URL_REGEX = /^([a-zA-Z]+:\/\/)?(www\.)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/.*)?(\?.*)?$/
 export const URL_MAX_LENGTH = 16384
