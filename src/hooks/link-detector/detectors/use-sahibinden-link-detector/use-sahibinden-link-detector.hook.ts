2bf534467c10de3614381269000bfc47d6f9c234
selimdoyranli <selimdoyranli@gmail.com>
2024-05-27 01:27:12 +0300

    fix: detector based link

--- src/hooks/link-detector/detectors/use-sahibinden-link-detector/use-sahibinden-link-detector.hook.ts
+++ src/hooks/link-detector/detectors/use-sahibinden-link-detector/use-sahibinden-link-detector.hook.ts
@@ -1,32 +1,6 @@
+import { isSahibindenDomain, isSahibindenIlan, getSahibindenIlanGecmisi } from '@/utils/detector/sahibinden.detector.util'
+
 export default () => {
-  const isSahibindenDomain = (url: string) => {
-    const _isSahibindenDomain = url.includes('sahibinden.com') || url.includes('shbd.io')
-
-    return _isSahibindenDomain
-  }
-
-  const isSahibindenIlan = (url: string) => {
-    const _isSahibindenIlan = /sahibinden\.com\/ilan\/[^/]+\/detay/.test(url)
-
-    return _isSahibindenIlan
-  }
-
-  const getSahibindenIlanGecmisi = async (ilanId: string) => {
-    try {
-      const response = await fetch(`/detector-based/fetch-announcement-price-history?provider=sahibinden&announcementId=${ilanId}`)
-      const result = await response.json()
-
-      return {
-        data: result.data,
-        error: null
-      }
-    } catch (error) {
-      console.error('Error fetching HTML data:', error)
-
-      throw error
-    }
-  }
-
   return {
     isSahibindenDomain,
     isSahibindenIlan,
